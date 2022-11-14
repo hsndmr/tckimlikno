db9cdc1e64c29e34d1e791f932e0b7abc56a5270
Hasan Demir <hsndmr5044@hotmail.com>
2022-11-14 03:31:58 +0300

    Add client for tc kimlik no validate service

---
+++ src/lib/client.spec.ts
@@ -0,0 +1,50 @@
+import axios from 'axios';
+
+import { Client } from './client';
+jest.mock('axios');
+
+class AxiosMock {
+  post() {
+    return Promise.resolve('test');
+  }
+}
+
+describe('Client', () => {
+  const args = {
+    name: 'name',
+    surname: 'surname',
+    tc: 11111111111,
+    birthYear: 2000,
+  };
+
+  it('should be defined', async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (axios.create as any).mockReturnValue(new AxiosMock());
+
+    const client = new Client();
+
+    const result = await client.post(args);
+
+    expect(result).toEqual('test');
+  });
+
+  it('getBody method should return soap body correctly', () => {
+    const client = new Client();
+
+    const result = client.getBody(args);
+
+    expect(
+      `<?xml version="1.0" encoding="utf-8"?>
+            <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+                <soap12:Body>
+                    <TCKimlikNoDogrula xmlns="http://tckimlik.nvi.gov.tr/WS">
+                    <TCKimlikNo>11111111111</TCKimlikNo>
+                    <Ad>name</Ad>
+                    <Soyad>surname</Soyad>
+                    <DogumYili>2000</DogumYili>
+                    </TCKimlikNoDogrula>
+                </soap12:Body>
+            </soap12:Envelope>`
+    ).toEqual(result);
+  });
+});
---
+++ src/lib/client.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosInstance } from 'axios';
+
+import { TCKimlikNoValidate } from './tc-kimlik-no-validate.interface';
+
+export class Client {
+  _axiosInstance: AxiosInstance;
+
+  constructor() {
+    this._axiosInstance = axios.create({
+      baseURL: 'https://tckimlik.nvi.gov.tr/service/kpspublic.asmx?WSDL',
+      headers: { 'Content-Type': 'application/soap+xml' },
+    });
+  }
+
+  post(args: TCKimlikNoValidate): Promise<string> {
+    return this._axiosInstance.post('', this.getBody(args));
+  }
+
+  getBody({ name, surname, tc, birthYear }: TCKimlikNoValidate) {
+    return `<?xml version="1.0" encoding="utf-8"?>
+            <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+                <soap12:Body>
+                    <TCKimlikNoDogrula xmlns="http://tckimlik.nvi.gov.tr/WS">
+                    <TCKimlikNo>${tc}</TCKimlikNo>
+                    <Ad>${name}</Ad>
+                    <Soyad>${surname}</Soyad>
+                    <DogumYili>${birthYear}</DogumYili>
+                    </TCKimlikNoDogrula>
+                </soap12:Body>
+            </soap12:Envelope>`;
+  }
+}
